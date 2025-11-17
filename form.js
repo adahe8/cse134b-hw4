d0580a7599dbe11d964cfa69d714fbe5b63b4652
adahe8 <ada.he0908@gmail.com>
2025-11-17 11:52:53 -0800

    Added placeholder link as example for LinkedIn field, extended invalid character checking to that field within js script. Also implemented info message reveals to provide additional form submission guidance for email and linkedin field, and to communicate state on submit.

--- form.js
+++ form.js
@@ -20,12 +20,24 @@ const form_errors = [];
 // temporary error message function
 // have default time shown be 3 secs
 function showErrorMsg(duration = 3000) {
-  const messageVal = document.querySelector("output p");
+  const messageVal = document.querySelector("#errors p");
   erroroutput.appendChild(messageVal);
   messageVal.style.opacity = "1";
   setTimeout(() => {
     messageVal.style.opacity = "0";
   }, duration - 500);
+}
+// temporary info message function, default time 3s
+function showInfoMsg(message, duration = 3000) {
+  const infoMessage = document.querySelector("#information p");
+  infoMessage.textContent = message;
+  infoMessage.style.opacity = "1";
+  setTimeout(() => {
+    infoMessage.style.opacity = "0";
+  }, duration - 500);
+  setTimeout(() => {
+    infoMessage.textContent = "";
+  }, duration);
 }
 
 // want to make required field go red after input field interacted with if left empty
@@ -54,9 +66,17 @@ inputs.forEach((field) => {
 });
 
 // applying warning styling and message in output section if improper char entered
-required.forEach((field) => {
+inputs.forEach((field) => {
   field.addEventListener("input", function () {
-    if (field.validity.patternMismatch) {
+    if (field === email || field === linkedin) {
+      let allowed = /^[a-zA-Z0-9._%+\-@/:]*$/;
+      if (!allowed.test(field.value)) {
+        showErrorMsg();
+        field.classList.add("invalid-entry");
+      } else {
+        field.classList.remove("invalid-entry");
+      }
+    } else if (field.validity.patternMismatch) {
       showErrorMsg();
       field.classList.add("invalid-entry");
     } else {
@@ -73,6 +93,7 @@ email.addEventListener("input", (event) => {
     );
   } else {
     email.setCustomValidity("");
+    showInfoMsg("Make sure your email is correct if you want a response!");
   }
 });
 
@@ -80,9 +101,13 @@ comments.addEventListener("input", (event) => {
   if (comments.value == "") {
     comments.setCustomValidity("A comment is required to submit this form.");
   } else {
-    comments.classList.remove("invalid");
     comments.setCustomValidity("");
+    comments.classList.remove("invalid");
   }
+});
+
+linkedin.addEventListener("focus", (event) => {
+  showInfoMsg("Copy in your profile link to connect!");
 });
 
 // counting down characters
@@ -104,5 +129,6 @@ comments.addEventListener("input", function () {
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   formErrors.value = JSON.stringify(form_errors);
+  showInfoMsg("Form submitted!");
   form.submit();
 });
